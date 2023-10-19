f37529c8530317d083f929d6db036e89bf1e5506
DELL <DELL@DESKTOP-4NB2LCS>
2023-10-19 18:25:35 +0300

    DATA:ADD Accountt

--- Move/Program.cs
+++ Move/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IService, Service>();
